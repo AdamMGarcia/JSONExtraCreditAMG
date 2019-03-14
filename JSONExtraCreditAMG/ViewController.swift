a2f07abf232e771e9028831e2fdf3c488e5e883a
Adam Garcia <amg16h@lib-301-24-9308.local>
2019-03-14 12:35:18 -0500

    Initial Commit

---
+++ JSONExtraCreditAMG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONExtraCreditAMG
+//
+//  Created by Adam Garcia on 3/14/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ JSONExtraCreditAMGTests/JSONExtraCreditAMGTests.swift
@@ -0,0 +1,34 @@
+//
+//  JSONExtraCreditAMGTests.swift
+//  JSONExtraCreditAMGTests
+//
+//  Created by Adam Garcia on 3/14/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import JSONExtraCreditAMG
+
+class JSONExtraCreditAMGTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
